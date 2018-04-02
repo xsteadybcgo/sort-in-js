1dbaff810deda1f9b6765d62cee6e1a2351112d0
xsteadybcgo <458520415@qq.com>
2018-04-02 10:30:26 +0800

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -1,10 +1,11 @@
-function bubble(){
+function bubble(array){
   var length = array.length;
   for (var i=0; i<length; i++){
     for (var j=0; j<length-1-i; j++ ){ //这里的减i是对bubble排序的一个小优化，避免掉上一个循环周期中不必要的比较
       if (array[j] > array[j+1]){
-        [arr[j], arr[j+1]] = [arr[j+1], arr[j]] 
+        [array[j], array[j+1]] = [array[j+1], array[j]] 
       }
     }
   }
+  return array;
 };
